a4a4c1684144e637e02fe9fe16c6dc631e1b15e6
vanesoledad86 <127568728+vanesoledad86@users.noreply.github.com>
2023-10-25 13:54:38 -0300

    Add files via upload

--- Visual Studio Ventas/js/cargarropa.js
+++ Visual Studio Ventas/js/cargarropa.js
@@ -1,7 +1,9 @@
-// Función para cargar tarjetas desde la API de FakeStore
+const PRECIODOLAR = 1000
+const opciones = { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 };
 async function cargarTarjetasDesdeAPI() {
     try {
         const response = await fetch('https://fakestoreapi.com/products');
+
         const data = await response.json();
 
         const tarjetasContainer = document.getElementById('tarjetas-container');
@@ -10,17 +12,20 @@ async function cargarTarjetasDesdeAPI() {
             const tarjeta = document.createElement('div');
             tarjeta.classList.add('tarjeta');
             tarjeta.innerHTML = `
-                <img src="${producto.image}" alt="${producto.title}">
-                <h2>${producto.title}</h2>
-                <p>Precio: $${producto.price}</p>
-                <p>Categoría: ${producto.category}</p>
-                <p>Descripción: ${producto.description}</p>
+                <a href="../index.html"><img src="${producto.image}" alt="${producto.title}">
+                <h3>${producto.title.substring(0, 25)}</h3>  
+                <p class="precio">$${(producto.price * PRECIODOLAR).toLocaleString('es-ES', opciones)}</p>
+                <p class="categoria">${title(producto.category)}</p>
+                <p class="desc">Descripción: ${producto.description.substring(0, 180)}</p></a>
             `;
             tarjetasContainer.appendChild(tarjeta);
         });
     } catch (error) {
         console.error('Error al cargar tarjetas desde la API:', error);
     }
+}
+function title(cadena) {
+    return cadena.charAt(0).toUpperCase() + cadena.slice(1);
 }
 
 // Llamar a la función para cargar las tarjetas cuando la página se cargue
